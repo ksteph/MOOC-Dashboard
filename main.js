50bee6391ea9f887cfff6429bac416a237ba7b27
Kate Hsiao <kamebkj@gmail.com>
2013-04-29 13:56:49 -0700

    layout version2

--- main.js
+++ main.js
@@ -1,57 +1,92 @@
 var margin = {top: 20, right: 20, bottom: 10, left: 50};
 
-var width = window.innerWidth/2.1 - margin.left - margin.right;
-var height = window.outerWidth/4.2 - margin.top - margin.bottom;
+var width = window.innerWidth/3 - margin.left - margin.right;
+var height = window.outerWidth/8 - margin.top - margin.bottom;
 
 /* Graded Items */
 for (var i=0; i<data.GradedItems.length; i++) {
-  /* Add divs */
-  var divMultiples = d3.select("body div#container-multiples").append("div")
-    .attr("id", "multiples"+i)
-    .attr("class", "multiples");
-  divMultiples.append("div").attr("id", "multiples-title"+i).attr("class", "multiples-title").text(data.GradedItems[i].title);
-  divMultiples.append("div").attr("id", "line-title"+i).attr("class", "line-title").text("Grade Distribution (Submitted)");  
-  divMultiples.append("div").attr("id", "multiples-pie"+i).attr("class", "multiples-pie");
-  divMultiples.append("div").attr("id", "multiples-line"+i).attr("class", "multiples-line");
-  divMultiples.append("div").attr("id", "multiples-bar"+i).attr("class", "multiples-bar");
-  
-  /* Add pie chart */
-  var pieChart0 = funcCreatePieChart(margin, height/1.5, width/2,
-                                      data.GradedItems[i].groups, "groups"+i);
-  var svg = d3.select("body div#multiples-pie"+i).append("svg");
-  pieChart0.DrawGraph(svg);
-  
-  /* Add line chart */
-  var gradedItem0 = funcCreateLineGraph(margin, height/2, width/2,
-                                        data.GradedItems[i].graph, "graded"+i);
-  gradedItem0.Scale.x.domain([0,100]);
-  var svg = d3.select("body div#multiples-line"+i).append("svg");
-  gradedItem0.DrawGraph(svg);
+  /* Add top & down divs */
+  var smallMultiples = d3.select("#container-multiple").append("div")
+    .attr("id", "small-multiple"+i)
+    .attr("class", "small-multiple");
+  smallMultiples.append("div").attr("id", "top-multiple"+i).attr("class", "top-multiple");
+  smallMultiples.append("div").attr("id", "down-multiple"+i).attr("class", "down-multiple");
 
-  /* Add stacked bar chart */
-  d3.select("body div#multiples-bar"+i).append("div").attr("id", "stackedbar-title"+i).attr("class", "stackedbar-title").text("Number of Attempts");
-  var stackedBar0 = funcCreateStackedBar(margin, height/6, width/1.1,
-                                        data.GradedItems[i].attempts, "stackedbar"+i, data.CurrentActiveStudents, data.MaxAttempts);
-  var svg = d3.select("body div#multiples-bar"+i).append("svg");
-  stackedBar0.DrawGraph(svg);
+  // Add 3 down divs, class name 'down'
+  var downMultiples = d3.select("#down-multiple"+i);
+  downMultiples.append("div").attr("id", "down1").attr("class","down")
+    .on("click", function(d){
+      console.log("bkj");
+    });
+  downMultiples.append("div").attr("id", "down2").attr("class","down");
+  downMultiples.append("div").attr("id", "down3").attr("class","down");
+
+  /* Add top multiple */ 
+  var top1 = funcCreateLineGraph(margin, height, width,
+                                        data.WeekActivity, "week-activity");
+  top1.XAxis.ticks(data.WeekActivity.length)
+  .tickFormat(function(d,i){
+    return data.WeekActivity[i].label;
+  });
+  svg = d3.select("#top-multiple"+i).append("svg");
+  top1.DrawGraph(svg);
+
+  /* Add bottom multiple */ 
+  var down1 = funcCreateLineGraph(margin, height/10, width/10,
+                                        data.WeekActivity, "week-activity");
+  down1.XAxis.ticks(data.WeekActivity.length)
+  .tickFormat(function(d,i){
+    return data.WeekActivity[i].label;
+  });
+  svg = d3.select("#down-multiple"+i+" #down1").append("svg");
+  down1.DrawGraph(svg);
+
+  var down2 = funcCreateLineGraph(margin, height/10, width/10,
+                                        data.WeekActivity, "week-activity");
+  down2.XAxis.ticks(data.WeekActivity.length)
+  .tickFormat(function(d,i){
+    return data.WeekActivity[i].label;
+  });
+  svg = d3.select("#down-multiple"+i+" #down2").append("svg");
+  down2.DrawGraph(svg);
+
+  /* Add pie chart */
+  // var pieChart0 = funcCreatePieChart(margin, height/1.5, width/2,
+  //                                     data.GradedItems[i].groups, "groups"+i);
+  // var svg = d3.select("body div#multiples-pie"+i).append("svg");
+  // pieChart0.DrawGraph(svg);
+  
+  // /* Add line chart */
+  // var gradedItem0 = funcCreateLineGraph(margin, height/2, width/2,
+  //                                       data.GradedItems[i].graph, "graded"+i);
+  // gradedItem0.Scale.x.domain([0,100]);
+  // var svg = d3.select("body div#multiples-line"+i).append("svg");
+  // gradedItem0.DrawGraph(svg);
+
+  // /* Add stacked bar chart */
+  // d3.select("body div#multiples-bar"+i).append("div").attr("id", "stackedbar-title"+i).attr("class", "stackedbar-title").text("Number of Attempts");
+  // var stackedBar0 = funcCreateStackedBar(margin, height/6, width/1.1,
+  //                                       data.GradedItems[i].attempts, "stackedbar"+i, data.CurrentActiveStudents, data.MaxAttempts);
+  // var svg = d3.select("body div#multiples-bar"+i).append("svg");
+  // stackedBar0.DrawGraph(svg);
 
 };
 
 
 
+///////////////////////
+
 /* Week Activity */
-var weekActivityTitle = d3.select("body div#container-activity").append("div").attr("id","title-weekActivity").text("Activity this week");
 var weekLineGraph = funcCreateLineGraph(margin, height, width,
                                         data.WeekActivity, "week-activity");
 weekLineGraph.XAxis.ticks(data.WeekActivity.length)
   .tickFormat(function(d,i){
     return data.WeekActivity[i].label;
   });
-svg = d3.select("body div#container-activity").append("svg");
+svg = d3.select("#activity-week-container").append("svg");
 weekLineGraph.DrawGraph(svg);
 
 /* Overall Activity */
-var overallActivityTitle = d3.select("body div#container-activity").append("div").attr("id","title-overallActivity").text("Active students");
 var overallLineGraph = funcCreateLineGraph(margin, height, width,
                                            data.OverallActivity,
                                            "overall-activity");
@@ -66,5 +101,13 @@ overallLineGraph.XAxis.ticks(data.OverallActivity.length)
     return date; /*not working yet*/
   });
 
-svg = d3.select("body div#container-activity").append("svg");
+svg = d3.select("#activity-overall-container").append("svg");
 overallLineGraph.DrawGraph(svg);
+
+////////////
+
+
+
+
+
+

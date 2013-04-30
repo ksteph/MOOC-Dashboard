73248559f046115e5b492b260c301c77348e045c
Kristin Stephens <ksteph@cs.berkeley.edu>
2013-04-30 14:28:36 -0700

    Add mini graph to multi-line-graph

--- multi_line_graph.js
+++ multi_line_graph.js
@@ -35,6 +35,11 @@ funcCreateMultiLineGraph = function(margin, height, width, data, tag,
   graph.XScaleDomain = x_range;
   graph.YScaleDomain = y_range;
 
+  graph.Mini = {
+    Data: graph.Data,
+    Tag: graph.Tag+"-mini"
+  };
+
   graph.Margin.axisLeft = graph.Margin.left + 50;
   graph.Margin.axisBottom = graph.Margin.bottom + 50;
 
@@ -62,6 +67,16 @@ funcCreateMultiLineGraph = function(margin, height, width, data, tag,
       .range(["#111","#c00","#0c0","#00c","#cc0","#0cc","#c0c","#f00", "#0f0", "#00f"])
   };
 
+  graph.Mini.Scale = {
+    x: d3.scale.linear()
+      .domain(graph.XScaleDomain),
+
+    y: d3.scale.linear()
+      .domain(graph.YScaleDomain),
+
+    color: graph.Scale.color
+  };
+
   // Set up axes
   graph.XAxis = d3.svg.axis()
     .scale(graph.Scale.x)
@@ -76,6 +91,103 @@ funcCreateMultiLineGraph = function(margin, height, width, data, tag,
     .x(function(d) { return graph.Scale.x(d.x); })
     .y(function(d) { return graph.Scale.y(d.y); });
 
+  graph.Mini.Line = d3.svg.line()
+    .x(function(d) { return graph.Mini.Scale.x(d.x); })
+    .y(function(d) { return graph.Mini.Scale.y(d.y); });
+
+
+  graph.DrawLines = function(graph, bInteractive) {
+    graph.SvgLineGroups = graph.SvgGroup.selectAll(".g-single-line-graph")
+      .data(graph.Data)
+      .enter().append("g")
+      .attr("class","g-single-line-graph")
+      .attr("id",function(d,i) { return graph.Tag+"-g-single-line-graph-"+i; });
+
+    graph.Lines = graph.SvgLineGroups.append("path")
+      .attr("class", "line-graph")
+      .attr("id", function(d,i) { return graph.Tag+"-line-"+i; })
+      .style("stroke", function(d,i) { return graph.Scale.color(i); })
+      .attr("d", function(d) { return graph.Line(d); });
+    
+    if (bInteractive) {
+      graph.Lines
+        .on("mouseover", function(d) {
+          svg_id = "#"+d3.select(this).node().parentNode.parentNode.parentNode.id;
+          
+          d3.select(svg_id).selectAll(".g-single-line-graph")
+            .style("opacity", 0.2);
+          
+          g_id = "#"+d3.select(this).node().parentNode.id;
+          
+          d3.select(g_id).style("opacity",1);
+        })
+        .on("mouseout", function(d) {
+          svg_id = "#"+d3.select(this).node().parentNode.parentNode.parentNode.id;
+          
+          d3.select(svg_id).selectAll(".g-single-line-graph")
+            .style("opacity", 1);
+        });
+    }
+    
+    if (bInteractive) {
+      // Find tooltip div or if not there create
+      graph.Tooltip = d3.select("#special-tooltip");
+      if (graph.Tooltip[0][0] == null) { // "[0][0]" Hacky but it works :-/
+        graph.Tooltip = d3.select("body")
+          .append("div")
+          .attr("id","special-tooltip")
+          .style("position", "absolute")
+          .style("z-index", "10")
+          .style("visibility", "hidden");
+      }
+    }
+    
+    // Create points on line graph
+    graph.Points = graph.SvgLineGroups.selectAll("point")
+      .data(function(d) { return d; })
+      .enter().append("g:circle")
+      .attr("class", "point")
+      .attr("id", graph.Tag+"-point")
+      .attr("cx", function(d,i){
+        return graph.Scale.x(d.x);
+      })
+      .attr("cy", function(d,i){
+        return graph.Scale.y(d.y);
+      })
+      .attr("r", 5)
+      .style("fill", function(d) { return graph.Scale.color(d.graph); })
+    
+    if (bInteractive) {
+      graph.Points
+        .on("mouseover", function(d){
+          d3.select(this).attr("opacity", "0.6");
+          graph.Tooltip.style("visibility", "visible")
+    	    .style("top", (event.pageY-10)+"px")
+            .style("left",(event.pageX+10)+"px")
+    	    .text(d.label);
+          
+          svg_id = "#"+d3.select(this).node().parentNode.parentNode.parentNode.id;
+          
+          d3.select(svg_id).selectAll(".g-single-line-graph")
+            .style("opacity", 0.2);
+          
+          g_id = "#"+d3.select(this).node().parentNode.id;
+          
+          d3.select(g_id).style("opacity",1);
+        })
+        .on("mouseout", function(d){
+          d3.select(this).attr("opacity", "1.0");
+          graph.Tooltip.style("visibility", "hidden")
+          
+          svg_id = "#"+d3.select(this).node().parentNode.parentNode.parentNode.id;
+          
+          d3.select(svg_id).selectAll(".g-single-line-graph")
+            .style("opacity", 1);
+        });
+    }
+    
+  };
+  
   graph.DrawGraph = function(svg) {
     graph = this;
     graph.Svg = svg;
@@ -108,86 +220,121 @@ funcCreateMultiLineGraph = function(margin, height, width, data, tag,
       .attr("dy", ".71em")
       .style("text-anchor", "end")
       .text("Number of students");
+
+    graph.DrawLines(graph, true);
     
     //   Draw Lines
-    graph.SvgLineGroups = graph.SvgGroup.selectAll(".g-single-line-graph")
-      .data(graph.Data)
-      .enter().append("g")
-      .attr("class","g-single-line-graph")
-      .attr("id",function(d,i) { return graph.Tag+"-g-single-line-graph-"+i; });
+    // graph.SvgLineGroups = graph.SvgGroup.selectAll(".g-single-line-graph")
+    //   .data(graph.Data)
+    //   .enter().append("g")
+    //   .attr("class","g-single-line-graph")
+    //   .attr("id",function(d,i) { return graph.Tag+"-g-single-line-graph-"+i; });
 
-    graph.SvgLineGroups.append("path")
-      .attr("class", "line-graph")
-      .attr("id", function(d,i) { return graph.Tag+"-line-"+i; })
-      .style("stroke", function(d,i) { return graph.Scale.color(i); })
-      .attr("d", function(d) { return graph.Line(d); })
-      .on("mouseover", function(d) {
-        svg_id = "#"+d3.select(this).node().parentNode.parentNode.parentNode.id;
+    // graph.SvgLineGroups.append("path")
+    //   .attr("class", "line-graph")
+    //   .attr("id", function(d,i) { return graph.Tag+"-line-"+i; })
+    //   .style("stroke", function(d,i) { return graph.Scale.color(i); })
+    //   .attr("d", function(d) { return graph.Line(d); })
+    //   .on("mouseover", function(d) {
+    //     svg_id = "#"+d3.select(this).node().parentNode.parentNode.parentNode.id;
 
-        d3.select(svg_id).selectAll(".g-single-line-graph")
-          .style("opacity", 0.2);
+    //     d3.select(svg_id).selectAll(".g-single-line-graph")
+    //       .style("opacity", 0.2);
 
-        g_id = "#"+d3.select(this).node().parentNode.id;
+    //     g_id = "#"+d3.select(this).node().parentNode.id;
 
-        d3.select(g_id).style("opacity",1);
-      })
-      .on("mouseout", function(d) {
-        svg_id = "#"+d3.select(this).node().parentNode.parentNode.parentNode.id;
+    //     d3.select(g_id).style("opacity",1);
+    //   })
+    //   .on("mouseout", function(d) {
+    //     svg_id = "#"+d3.select(this).node().parentNode.parentNode.parentNode.id;
 
-        d3.select(svg_id).selectAll(".g-single-line-graph")
-          .style("opacity", 1);
-      });
+    //     d3.select(svg_id).selectAll(".g-single-line-graph")
+    //       .style("opacity", 1);
+    //   });
 
-    //   Find tooltip div or if not there create
-    graph.Tooltip = d3.select("#special-tooltip");
-    if (graph.Tooltip[0][0] == null) { // "[0][0]" Hacky but it works :-/
-      graph.Tooltip = d3.select("body")
-        .append("div")
-        .attr("id","special-tooltip")
-        .style("position", "absolute")
-        .style("z-index", "10")
-        .style("visibility", "hidden");
-    }
+    // //   Find tooltip div or if not there create
+    // graph.Tooltip = d3.select("#special-tooltip");
+    // if (graph.Tooltip[0][0] == null) { // "[0][0]" Hacky but it works :-/
+    //   graph.Tooltip = d3.select("body")
+    //     .append("div")
+    //     .attr("id","special-tooltip")
+    //     .style("position", "absolute")
+    //     .style("z-index", "10")
+    //     .style("visibility", "hidden");
+    // }
     
-    //   Create points on line graph
-    graph.SvgLineGroups.selectAll("point")
-      .data(function(d) { return d; })
-      .enter().append("g:circle")
-      .attr("class", "point")
-      .attr("id", graph.Tag+"-point")
-      .attr("cx", function(d,i){
-        return graph.Scale.x(d.x);
-      })
-      .attr("cy", function(d,i){
-        return graph.Scale.y(d.y);
-      })
-      .attr("r", 5)
-      .style("fill", function(d) { return graph.Scale.color(d.graph); })
-      .on("mouseover", function(d){
-        d3.select(this).attr("opacity", "0.6");
-        graph.Tooltip.style("visibility", "visible")
-    	  .style("top", (event.pageY-10)+"px")
-          .style("left",(event.pageX+10)+"px")
-    	  .text(d.label);
+    // //   Create points on line graph
+    // graph.SvgLineGroups.selectAll("point")
+    //   .data(function(d) { return d; })
+    //   .enter().append("g:circle")
+    //   .attr("class", "point")
+    //   .attr("id", graph.Tag+"-point")
+    //   .attr("cx", function(d,i){
+    //     return graph.Scale.x(d.x);
+    //   })
+    //   .attr("cy", function(d,i){
+    //     return graph.Scale.y(d.y);
+    //   })
+    //   .attr("r", 5)
+    //   .style("fill", function(d) { return graph.Scale.color(d.graph); })
+    //   .on("mouseover", function(d){
+    //     d3.select(this).attr("opacity", "0.6");
+    //     graph.Tooltip.style("visibility", "visible")
+    // 	  .style("top", (event.pageY-10)+"px")
+    //       .style("left",(event.pageX+10)+"px")
+    // 	  .text(d.label);
 
-        svg_id = "#"+d3.select(this).node().parentNode.parentNode.parentNode.id;
+    //     svg_id = "#"+d3.select(this).node().parentNode.parentNode.parentNode.id;
 
-        d3.select(svg_id).selectAll(".g-single-line-graph")
-          .style("opacity", 0.2);
+    //     d3.select(svg_id).selectAll(".g-single-line-graph")
+    //       .style("opacity", 0.2);
 
-        g_id = "#"+d3.select(this).node().parentNode.id;
+    //     g_id = "#"+d3.select(this).node().parentNode.id;
 
-        d3.select(g_id).style("opacity",1);
-      })
-      .on("mouseout", function(d){
-        d3.select(this).attr("opacity", "1.0");
-        graph.Tooltip.style("visibility", "hidden")
+    //     d3.select(g_id).style("opacity",1);
+    //   })
+    //   .on("mouseout", function(d){
+    //     d3.select(this).attr("opacity", "1.0");
+    //     graph.Tooltip.style("visibility", "hidden")
 
-        svg_id = "#"+d3.select(this).node().parentNode.parentNode.parentNode.id;
+    //     svg_id = "#"+d3.select(this).node().parentNode.parentNode.parentNode.id;
 
-        d3.select(svg_id).selectAll(".g-single-line-graph")
-          .style("opacity", 1);
-      });
+    //     d3.select(svg_id).selectAll(".g-single-line-graph")
+    //       .style("opacity", 1);
+    //   });
+  };
+
+  // This scaling is not quite right, but the best I can do.
+  graph.DrawMiniGraph = function(svg, height, width, margin) {
+    graph = this;
+    graph.Mini.Svg = svg;
+    graph.Mini.Height = height;
+    graph.Mini.Width = width;
+    graph.Mini.Margin = margin;
+
+    var scaleX = graph.Mini.Width/graph.Width;
+    var scaleY = graph.Mini.Height/graph.Height;
+    console.log(scaleX);
+    console.log(scaleY);
+
+    // graph.Mini.Scale.x.range([0,graph.Mini.Width-graph.Mini.Margin.left-
+    //                           graph.Mini.Margin.right]);
+    // graph.Mini.Scale.y.range([graph.Mini.Height-graph.Mini.Margin.bottom,0]);
+    graph.Mini.Scale.x.range([
+      0,
+      graph.Width-((graph.Mini.Margin.left+graph.Mini.Margin.right)/scaleX)
+    ]);
+    graph.Mini.Scale.y.range([graph.Height-((graph.Mini.Margin.bottom+graph.Mini.Margin.top)/scaleY), 0]),
+
+    graph.Mini.SvgGroup = graph.Mini.Svg
+      .attr("id", graph.Mini.Tag+"-line-graph")
+      .attr("width", graph.Mini.Width)
+      .attr("height", graph.Mini.Height)
+      .append("g")
+      .attr("transform", "translate(" + graph.Mini.Margin.left + "," + 
+            graph.Mini.Margin.top + ") scale("+scaleX+","+scaleY+")");
+
+    graph.DrawLines(graph.Mini, false);
   };
 
   return graph;
